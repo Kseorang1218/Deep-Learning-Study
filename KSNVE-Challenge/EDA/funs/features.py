6cc0293986e9e9d4f9bb63b48f01d70d16c2cac1
Kseorang1218 <switch1218@uos.ac.kr>
2024-11-08 12:55:43 +0900

    🚧  Avg, peak, p2p, crest factor plot

--- KSNVE-Challenge/EDA/funs/features.py
+++ KSNVE-Challenge/EDA/funs/features.py
@@ -2,18 +2,30 @@
 
 import numpy as np
 
+def average(data):
+    average = np.mean(data)
+    return average
+
 def rms(data):
-    """_summary_
-
-    Parameters
-    ----------
-    data : _type_
-        _description_
-
-    Returns
-    -------
-    _type_
-        _description_
-    """
     rms = np.sqrt(np.mean(data**2))
+
     return rms
+
+def peak(data):
+    peaks = np.max(np.abs(data))
+
+    return peaks
+
+def peak_to_peak(data):
+    x_max = np.max(data)
+    x_min = np.min(data)
+    pk2pk = x_max - x_min
+
+    return pk2pk
+
+def crest_factor(data):
+    peaks = np.max(np.abs(data))
+    rms = np.sqrt(np.mean(data**2))
+    crest_factor = peaks/rms
+
+    return crest_factor
--- KSNVE-Challenge/EDA/funs/painter.py
+++ KSNVE-Challenge/EDA/funs/painter.py
@@ -10,7 +10,7 @@ title_size = 20
 label_size = 15
 
 def draw_hist(
-        df: pd.DataFrame, title: str, xlabel: str, ylabel: str, xlim: Tuple = None
+        df: pd.DataFrame, statistics: str, title: str, xlabel: str, ylabel: str, xlim: Tuple = None
         ) -> None:
     """
     데이터프레임으로부터 히스토그램을 그리는 함수
@@ -19,6 +19,8 @@ def draw_hist(
     ----------
     df : pd.DataFrame
         히스토그램을 그릴 데이터프레임
+    statistics: str
+        그리고자 하는 통계값
     title : str
         그림 제목
     xlabel : str
@@ -30,7 +32,7 @@ def draw_hist(
     """
     for fault_type in ['normal', 'ball', 'inner', 'outer']:
         sub_df = df[df['fault_type'] == fault_type]
-        plt.hist(sub_df['rms'], bins=100, alpha=0.7, label=fault_type)
+        plt.hist(sub_df[statistics], bins=100, alpha=0.7, label=fault_type)
 
     plt.xlim(xlim)
     plt.title(title, fontsize=title_size)
